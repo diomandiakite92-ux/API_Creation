bad4131f7c0cb0f75143195a33744d0fa1791b9f
diomandiakite92-ux <diomandiakite92@gmail.com>
2026-06-09 21:41:52 +0200

    Création des 1ères routes et ajout de Nodemon

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// @ts-ignore
+const data = [];
+const port = process.env.PORT || 5000;
+
+/** @type {any[]} */
+let posts = [];
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/posts", (req, res) => {
+  // @ts-ignore
+  const data_posts = [...posts, ...data];
+  res.json(data_posts);
+});
+
+app.post(`/posts/create`, (req, res) => {
+  posts = [...posts, ...req.body];
+  res.send("New post created");
 });
 
 app.listen(port, () => {
